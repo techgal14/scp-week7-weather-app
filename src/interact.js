3e3bc22807b3f5dbd7af966049be10ec551ca116
techgal14 <189483818+techgal14@users.noreply.github.com>
2025-01-26 10:33:12 -0800

    changes
    - added Axios
    - enlarged size of text box
    - positioning

--- src/interact.js
+++ src/interact.js
@@ -1,8 +1,15 @@
+function searchCity(city) {
+  let apiKey = "4a7bf3d9cod778ee0bbe6f174ftf4fa8";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+
+}
 function handleSearchSubmit(event) {
   event.preventDefault();
   let searchInput = document.querySelector("#search-form-input");
   let cityElement = document.querySelector("#city");
   cityElement.innerHTML = searchInput.value;
+  searchCity(searchInput.value);
+  console.log(searchInput.value);
 }
 
 let searchFormElement = document.querySelector("#search-form");
